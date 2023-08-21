002fea6ca15a8b7a76ea8ca1eb33b15ac092b921
Scott McKissock <scottmckissock@gmail.com>
2023-08-21 14:56:04 -0400

    Scrape multiple pages

--- scrape.js
+++ scrape.js
@@ -1,22 +1,15 @@
 const cheerio = require("cheerio");
 const fs = require("fs"); //import { writeFile } from 'fs/promises';
 
+let $;
 
-async function scrape() {
-    
-    const resp = await fetch("https://www.democracydocket.com/cases/");
-    const text = await resp.text();
+function getPage(cases) {
 
-    const $ = cheerio.load(text);
-
-    const cases = [];    
-    $(".archive-card").each((i, e) => {
+    $(".archive-card").each((i, e) => {        
         let aCase = {};
 
         // state, stateImg 
-        const $stateDiv = $(e).find(".archive-card__state");
-        // const state = $($stateDiv).find("img").attr("alt").replace("State of ", "").trim();
-        // const stateImg = $($stateDiv).find("img").attr("src").trim();
+        const $stateDiv = $(e).find(".archive-card__state");       
         aCase.state = $($stateDiv).find("img").attr("alt").replace("State of ", "").trim();
         aCase.stateImg = $($stateDiv).find("img").attr("src").trim();
 
@@ -24,23 +17,18 @@ async function scrape() {
         const $caseDiv = $(e).find(".archive-card__main");
         const $caseTagUl = $($caseDiv).find(".post__list-tags");
         const $caseTagLis = $($caseTagUl).find("li");
-
         $caseTagLis.each((i, el) => {
             const tag = $(el).find("li a").text().trim();
         });
+    
+        aCase.parties = $(e).find("p.archive-card__parties").text().trim();                      
 
-        //const parties = $(e).find("p.archive-card__parties").text().trim();
-        aCase.parties = $(e).find("p.archive-card__parties").text().trim();              
-
-        const $excerptDiv = $(e).find("div.archive-card__excerpt")
-        //const excerpt = $($excerptDiv).find("p").text().trim();
+        const $excerptDiv = $(e).find("div.archive-card__excerpt");        
         aCase.excerpt = $($excerptDiv).find("p").text().trim();
-      
+  
         const $metaDiv = $(e).find("div.archive-card__meta");
         const $datePs = $($metaDiv).find(".archive-card__meta p");
 
-        //let dateFiled = "";
-        //let dateDecided = "";
         $datePs.each((i, el) => {            
             if (i == 0) 
                 aCase.dateFiled = $(el).find("time").attr("datetime");
@@ -48,22 +36,39 @@ async function scrape() {
                 aCase.dateDecided = $(el).find("time").attr("datetime");
         });
 
-        cases.push(aCase);
-        
-        // cases.push({
-        //     state,
-        //     stateImg,
-        //     dateFiled,
-        //     dateDecided,
-        //     parties,
-        //     excerpt,
-        // })
-        
+        cases.push(aCase);      
     });
+    return cases;
+};
 
-    //console.log(items.length)
-    //console.log(items[0])
-   
+
+async function fetchPage(url) {    
+    const resp = await fetch(url);
+    const text = await resp.text();
+
+    return text;
+    $ = cheerio.load(text);
+}
+
+
+async function scrape() {    
+    let cases = [];
+
+    const PAGES = 7; //64;
+
+    for (i = 2; i <= PAGES; i++) {
+        let url = "https://www.democracydocket.com/cases";
+        if (i > 1)
+            url = `https://www.democracydocket.com/cases/page/${i}`;
+
+        await fetchPage(url)
+        .then((text) => {
+            $ = cheerio.load(text);
+            cases = getPage(cases)
+            console.log("CASES " + cases.length);
+        });
+    }
+        
     let csvStrings = [`state,stateImg,parties,dateFiled,dateDecided,excerpt`];
     cases.forEach(d => {
         csvStrings.push(`${d.state},${d.stateImg},"${d.parties}",${d.dateFiled},${d.dateDecided},"${d.excerpt }"`);
